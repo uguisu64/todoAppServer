f19a6249d14853fbb264f659c4b23f6604c62376
uguisu64 <soma64soma@gmail.com>
2022-06-06 11:19:26 +0900

    DB接続コード追加

--- src/Application.kt
+++ src/Application.kt
@@ -4,8 +4,18 @@ import io.ktor.response.*
 import io.ktor.routing.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
+import org.jetbrains.exposed.sql.Database
+import org.jetbrains.exposed.sql.transactions.TransactionManager
+import java.sql.Connection
 
 fun main() {
+    //sqliteのDataBaseファイルのパス
+    val dbPath = "jdbc:sqlite:C:\\src\\sqlite3\\test.db"
+
+    //sqliteへの接続
+    Database.connect(dbPath,"org.sqlite.JDBC")
+    TransactionManager.manager.defaultIsolationLevel = Connection.TRANSACTION_SERIALIZABLE
+
     val server = embeddedServer(Netty, port = 8080) {
         routing {
             get("/") {
