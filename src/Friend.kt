1b35d406a42fc416e15bc94c70d254148c86dfc5
tatsu219 <tatsu0219007@gmail.com>
2022-06-21 15:34:26 +0900

    6/21 表題コメント追加

--- src/Friend.kt
+++ src/Friend.kt
@@ -1,3 +1,18 @@
+/*
+*** File Name    :Friend.kt
+*** Designer     :平出　達大
+*** Date         :2022.06.21
+*** Purpose      :フレンド関係の処理
+ */
+
+/*
+*** 2022.06.01 : fristcommit
+*** 2022.06.21 : friendlist修正
+*** 2022.06.21 : addfriend追加
+*** 2022.06.21 : deletefriend追加
+*** 2022.06.21 : 表題コメント追加
+ */
+
 import org.jetbrains.exposed.sql.*
 import org.jetbrains.exposed.sql.transactions.transaction
 import dsl.User
--- src/dsl/FriendTable.kt
+++ src/dsl/FriendTable.kt
@@ -1,5 +1,17 @@
-package dsl
+/*
+*** File Name    :FriendTable.kt
+*** Designer     :平出　達大
+*** Date         :2022.06.21
+*** Purpose      :フレンドTable作成
+ */
 
+/*
+*** 2022.06.01 : fristcommit
+*** 2022.06.21 : FriendTable修正
+*** 2022.06.21 : 表題コメント追加
+ */
+
+package dsl
 import org.jetbrains.exposed.sql.Table
 
 object FriendTable : Table() {
