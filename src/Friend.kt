c532dd40d7311ea271e3d7c4c0263652fa21824d
tatsu219 <tatsu0219007@gmail.com>
2022-07-03 15:24:16 +0900

    7/3 Friend.kt 修正

--- src/Friend.kt
+++ src/Friend.kt
@@ -77,29 +77,31 @@ class Friend () {
         return applys;
     }
 
-
-    //相手のidを入れる。 table
-    //OK だったらtableに入れる
-    fun apply(myid : Int, friendid : Int){  //申請許可
+    fun FriendApply(myid : Int, friendId: Int){ //フレンド申請
         transaction {
-            val friendId = FriendTable.select{ (FriendApplyTable.Myid eq myid) }.single()[FriendApplyTable.Friendid]
-            if(friendid == friendId){  //FriendApplyTableにあったら実行
-                FriendTable.insert { //FriendTableに書き込む
-                    it[UserId] = myid
-                    it[Friendid] = friendId
-                }
-            }
-
             FriendApplyTable.insert {  //フレンド申請の許可、不許可に使う
                 it[Myid] = myid
                 it[Friendid] = friendId
             }
-            FriendApplyTable.deleteWhere { (FriendApplyTable.Myid eq myid) and (FriendApplyTable.Friendid eq friendid)}
-        }// FriendApplyTableからFriendTableに書き込んだ情報を消去
-
+        }
     }
 
-    fun disapproval(myid : Int, friendid : Int){  // 申請不許可
+    //相手のidを入れる。 FriendApplytable
+    //OK だったらFriendtableに入れる
+    fun apply(myid : Int, friendid : Int){  //申請許可
+        transaction {
+            val record = FriendApplyTable.select{ (FriendApplyTable.Myid eq myid) and (FriendApplyTable.Friendid eq friendid)}.single()
+            if(record[FriendApplyTable.Myid] == myid && record[FriendApplyTable.Friendid] == friendid){
+                FriendTable.insert { //FriendTableに書き込む
+                    it[UserId] = myid
+                    it[Friendid] = friendid
+                }
+            }
+            FriendApplyTable.deleteWhere { (FriendApplyTable.Myid eq myid) and (FriendApplyTable.Friendid eq friendid)}
+        }
+    }// FriendApplyTableから申請を行ったレコードを消去する
+
+    fun disapproval(myid : Int, friendid : Int){  // 申請拒否
         transaction {
             FriendApplyTable.deleteWhere { (FriendApplyTable.Myid eq myid) and (FriendApplyTable.Friendid eq friendid)}
         }  //FriendApplyTableから不許可した情報を消去
